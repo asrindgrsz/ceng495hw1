559b51feaa7c4530d80034c8f1428232a2dd5eb2
Asrın Doğrusöz <148554817+asrindgrsz@users.noreply.github.com>
2024-04-26 01:00:00 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,15 @@
 from flask import Flask
+from pymongo import MongoClient
+import os
+import logging
+from datetime import datetime
+from  bson.objectid import ObjectId
+
 app = Flask(__name__)
+
+logging.basicConfig(filename='app.log', level=logging.ERROR)
+
+MONGO_URI = "mongodb+srv://e2380301:ceng495hw1@cluster0.9pjmxxw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 @app.route('/')
 def hello_world():
